b5d16c50411aa2d5af19c38a08756fcf09a54d8c
yu.2479 <yu.2479@osu.edu>
2021-11-29 16:11:30 -0500

    Yiqing's final push

--- app/src/androidTest/java/com/app/hallselector/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/app/hallselector/ExampleInstrumentedTest.java
@@ -1,13 +1,28 @@
 package com.app.hallselector;
 
+import static android.os.Trace.isEnabled;
+
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +32,32 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.app.hallselector", appContext.getPackageName());
     }
+
+    @Test
+    public void buttonIsEnabled() {
+
+        Espresso.onView(withId(R.id.new_search_button)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void buttonIsDisplayed() {
+        Espresso.onView(withId(R.id.new_search_button)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void buttonIsClickable() {
+        Espresso.onView(withId(R.id.new_search_button)).check(matches((isClickable())));
+    }
+
 }
